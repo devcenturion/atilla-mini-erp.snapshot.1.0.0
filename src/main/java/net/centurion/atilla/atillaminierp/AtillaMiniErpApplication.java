336783dd53d5870b502c75422685a6eba557e968
devcenturion <devcenturion@gmail.com>
2019-03-26 15:12:53 +0100

    Initial commit

---
+++ src/main/java/net/centurion/atilla/atillaminierp/AtillaMiniErpApplication.java
@@ -0,0 +1,13 @@
+package net.centurion.atilla.atillaminierp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtillaMiniErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtillaMiniErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/centurion/atilla/atillaminierp/AtillaMiniErpApplicationTests.java
@@ -0,0 +1,16 @@
+package net.centurion.atilla.atillaminierp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtillaMiniErpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
